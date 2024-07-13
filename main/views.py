be618be375dd95d1227a58c8046364ae22f52fb6
GGufficiale <germangranovskii@mail.ru>
2024-07-13 19:07:36 +0300

    19.2 Обработка входящих запросов - реализовано

--- main/views.py
+++ main/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Метод для обработки данных, получаемых на фротэнде при вводе пользователем"""
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
     return render(request, 'main/index.html')
-
